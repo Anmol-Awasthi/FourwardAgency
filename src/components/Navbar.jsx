a4d7d205fad1fe3ee4a0404c314169b5e16aba9a
Anmol <anmolawasthi777@gmail.com>
2024-07-11 12:12:02 +0530

    Made the Navbar responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,25 +5,28 @@ import AnimatedText from "./AnimatedText";
 const Navbar = ({ color = "text-gray-200", customClassesForText, lineColor = "white" }) => {
   const navItems = [
     { name: "WORK", path: "/work" },
-    { name: "SERVICES", path: "/services" },
+    { name: "SERVICES", path: "/services", mobileHide: true },
     { name: "FOURWARD", path: "/" },
-    { name: "ABOUT", path: "/about" },
+    { name: "ABOUT", path: "/about", mobileHide: true },
     { name: "CONTACT", path: "/contact" },
   ];
 
   return (
-    <nav className="fixed top-0 left-0 w-full h-16 bg-transparent z-50 text-gray-700 font-formula bg-cyan-500 tracking-[0.5rem] text-[1.5rem] font-bold">
+    <nav className="fixed top-0 left-0 w-full mt-4 sm:mt-0 h-16 bg-transparent z-50 text-gray-700 font-formula bg-cyan-500 tracking-[0.5rem] text-[1.5rem] font-bold">
       <ul className="flex items-center justify-between mx-6 gap-10 h-full tracking-[0.1rem] text-white text-2xl font-bold">
         {navItems.map((item, index) => {
           let itemClasses;
           if (item.name === "FOURWARD") {
-            itemClasses = `${color} text-3xl tracking-widest opacity-90 font-formula font-extrabold hover:text-white hover:opacity-100`;
+            itemClasses = `${color} text-5xl sm:text-3xl tracking-widest opacity-90 font-formula font-extrabold hover:text-white hover:opacity-100`;
           } else {
-            itemClasses = `${color} opacity-80 text-xl font-medium hover:text-white hover:opacity-100`;
+            itemClasses = `${color} opacity-80 text-3xl sm:text-xl font-medium hover:text-white hover:opacity-100`;
           }
 
+          // Add hidden class to items with mobileHide true
+          const mobileHideClass = item.mobileHide ? "hidden md:block" : "";
+
           return (
-            <li key={index}>
+            <li key={index} className={mobileHideClass}>
               <NavLink
                 exact
                 to={item.path}
--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '192.168.1.2',  // Listen on all network interfaces
+    port: 5173,       // You can change this port if needed
+  }
 })
